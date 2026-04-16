e359d38152032d57158987b425df89758f48b67f
b3nnnya <b.sanhueza05@ufromail.cl>
2026-04-16 09:31:16 -0400

    Controller Commit

--- src/main/java/Controlador/ResultadoController.java
+++ src/main/java/Controlador/ResultadoController.java
@@ -1,4 +1,18 @@
 package Controlador;
 
+import Modelo.Resultado;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ResultadoController {
+
+    private final List<Resultado> historial = new ArrayList<>();
+
+    public void agregarResultado(Resultado r) {
+        historial.add(r);
+    }
+
+    public List<Resultado> getHistorial() {
+        return historial;
+    }
 }
--- src/main/java/Controlador/RuletaController.java
+++ src/main/java/Controlador/RuletaController.java
@@ -1,4 +1,29 @@
 package Controlador;
 
+import Modelo.*;
+
 public class RuletaController {
+
+    private final Ruleta ruleta;
+
+    public RuletaController(Ruleta ruleta) {
+        this.ruleta = ruleta;
+    }
+
+    public Resultado jugar(int monto, TipoApuesta tipo) {
+        int numero = ruleta.girar();
+        boolean gano = ruleta.evaluar(numero, tipo);
+
+        ruleta.apostar(monto, gano);
+
+        return new Resultado(numero, tipo, monto, gano);
+    }
+
+    public int getSaldo() {
+        return ruleta.getSaldo();
+    }
+
+    public void depositar(int monto) {
+        ruleta.depositar(monto);
+    }
 }
