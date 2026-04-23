56a0c17ba7dc91d829f4ff0d88405d164fd4efec
GatoMiau03 <d.benaldo03@ufromail.cl>
2026-04-23 10:00:30 -0400

    Correccion en ResultadoController

--- src/main/java/Controlador/ResultadoController.java
+++ src/main/java/Controlador/ResultadoController.java
@@ -1,18 +1,17 @@
 package Controlador;
 
 import Modelo.Resultado;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ResultadoController {
 
-    private final List<Resultado> historial = new ArrayList<>();
+    private final SessionController session;
 
-    public void agregarResultado(Resultado r) {
-        historial.add(r);
+    public ResultadoController(SessionController session) {
+        this.session = session;
     }
 
-    public List<Resultado> getHistorial() {
-        return historial;
+    public List<Resultado> obtenerHistorial() {
+        return session.getUsuario().getHistorial();
     }
 }
