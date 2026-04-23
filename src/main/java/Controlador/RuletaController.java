76c7da529a0adf7666984d0e2a2ddc34f50a3f4a
GatoMiau03 <d.benaldo03@ufromail.cl>
2026-04-23 09:01:21 -0400

    Modificacion en RuletaController

--- src/main/java/Controlador/RuletaController.java
+++ src/main/java/Controlador/RuletaController.java
@@ -5,18 +5,29 @@ import Modelo.*;
 public class RuletaController {
 
     private final Ruleta ruleta;
+    private final SessionController session;
 
-    public RuletaController(Ruleta ruleta) {
+    public RuletaController(Ruleta ruleta, SessionController session) {
         this.ruleta = ruleta;
+        this.session = session;
     }
 
     public Resultado jugar(int monto, TipoApuesta tipo) {
+
         int numero = ruleta.girar();
         boolean gano = ruleta.evaluar(numero, tipo);
 
         ruleta.apostar(monto, gano);
 
-        return new Resultado(numero, tipo, monto, gano);
+        Resultado r = new Resultado(numero, tipo, monto, gano);
+
+        // 🔥 GUARDAR EN RUETA
+        ruleta.registrarResultado(r);
+
+        // 🔥 GUARDAR EN USUARIO
+        session.getUsuario().agregarResultado(r);
+
+        return r;
     }
 
     public int getSaldo() {
