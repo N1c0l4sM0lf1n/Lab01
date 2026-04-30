7642bd12f83b95a0dff2dd7d5f70549828b0619d
dci <n.molfinqueo01@ufromail.cl>
2026-04-30 09:43:32 -0400

    Actualizacion Estadisticas

--- src/main/java/Modelo/Estadisticas.java
+++ src/main/java/Modelo/Estadisticas.java
@@ -1,9 +1,11 @@
 package Modelo;
 
+import java.util.*;
+
 public class Estadisticas {
 
-	private int TotalVictorias;
-	private int Victorias;
+	private int totalJugadas;
+	private int victorias;
 	private double porcentajeVictorias;
 	private int rachaMaxima;
 	private TipoApuesta tipoMasJugado;
