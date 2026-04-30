38c129e3a140be91d6e3bc0571fa2b5b4cceb684
dci <n.molfinqueo01@ufromail.cl>
2026-04-30 09:42:08 -0400

    Creacion Clase Estadisticas

---
+++ src/main/java/Modelo/Estadisticas.java
@@ -0,0 +1,11 @@
+package Modelo;
+
+public class Estadisticas {
+
+	private int TotalVictorias;
+	private int Victorias;
+	private double porcentajeVictorias;
+	private int rachaMaxima;
+	private TipoApuesta tipoMasJugado;
+
+}
