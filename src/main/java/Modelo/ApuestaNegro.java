d3f6afb2e67aca75e56e5b67fae1614c1ebffe0c
GatoMiau03 <d.benaldo03@ufromail.cl>
2026-05-07 09:20:32 -0400

    ApuestaNegro

--- src/main/java/Modelo/ApuestaNegro.java
+++ src/main/java/Modelo/ApuestaNegro.java
@@ -1,4 +1,13 @@
 package Modelo;
 
-public class ApuestaNegro {
+public class ApuestaNegro extends ApuestaBase {
+
+    public ApuestaNegro(int monto) {
+        super(monto, "Negro");
+    }
+
+    @Override
+    public boolean acierta(int numero, String color) {
+        return numero != 0 && color.equals("Negro");
+    }
 }
