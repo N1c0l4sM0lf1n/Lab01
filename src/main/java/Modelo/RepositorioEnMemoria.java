0a0690e5c83305b4018681fbf00714ac0a5c3a1c
Gabriel <g,valenzuela10@ufromail.com>
2026-05-28 09:42:06 -0400

    RepositorioEnMemoria

--- src/main/java/Modelo/RepositorioEnMemoria.java
+++ src/main/java/Modelo/RepositorioEnMemoria.java
@@ -1,4 +1,21 @@
 package Modelo;
 
-public class RepositorioEnMemoria {
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepositorioEnMemoria
+        implements IRepositorioResultados {
+
+    private final List<Resultado> resultados =
+            new ArrayList<>();
+
+    @Override
+    public void guardarResultado(Resultado resultado) {
+        resultados.add(resultado);
+    }
+
+    @Override
+    public List<Resultado> obtenerResultados() {
+        return resultados;
+    }
 }
