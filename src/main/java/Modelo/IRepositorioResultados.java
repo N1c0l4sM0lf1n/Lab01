13d2ea00ad2874528e0644f3cdec4f29b18f1fc1
Gabriel <g,valenzuela10@ufromail.com>
2026-05-28 09:41:15 -0400

    Interfaz Repositorio

--- src/main/java/Modelo/IRepositorioResultados.java
+++ src/main/java/Modelo/IRepositorioResultados.java
@@ -1,4 +1,10 @@
 package Modelo;
 
-public class IRepositorioResultados {
+import java.util.List;
+
+public interface IRepositorioResultados {
+
+    void guardarResultado(Resultado resultado);
+
+    List<Resultado> obtenerResultados();
 }
